5c89780cb875709f761cfc1e8e3620eee9092843
BuildTools <unconfigured@null.spigotmc.org>
2023-12-18 00:08:16 +0100

    misc idk

--- src/main/java/com/gutscheweb/castles/game/Game.java
+++ src/main/java/com/gutscheweb/castles/game/Game.java
@@ -1,33 +1,75 @@
 package com.gutscheweb.castles.game;
 
 import com.gutscheweb.castles.Castles;
+import com.sk89q.worldedit.bukkit.adapter.BukkitImplAdapter;
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.bossbar.BossBar;
+import net.kyori.adventure.bossbar.BossBarViewer;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static com.gutscheweb.castles.Castles.instance;
 
 public class Game {
     private static final MiniMessage mm = MiniMessage.miniMessage();
 
     public static void startCountDown(Player player, int max) {
-        final AtomicInteger[] sec = {new AtomicInteger()};
-        Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Castles.instance,
-                () -> {
-                    player.showBossBar(updateBossBar(sec[0].get(), max));
+
+        final BossBar bar = BossBar.bossBar(
+                mm.deserialize(max + "s remaining"),
+                1-getProgress(0, max),
+                BossBar.Color.YELLOW,
+                BossBar.Overlay.PROGRESS
+        );
+        player.showBossBar(bar);
+        BukkitRunnable runnable = new BukkitRunnable() {
+            int current = 0;
+            @Override
+            public void run() {
+                bar.name(mm.deserialize(max - current + "s remaining"));
+                bar.progress(1-getProgress(current, max));
+                current++;
+                if (current > max) {
+                    bar.viewers().forEach(v -> bar.removeViewer((Audience) v));
+                    this.cancel();
+                }
+            }
+        };
+
+        runnable.runTaskTimer(instance, 0L, 20L);
+
+        /*
+        * () -> {
+                    if (max >= sec[0].get()) Bukkit.getScheduler().cancelTask(tid);
+                    for (BossBar bar : player.activeBossBars()) {
+                        if (bar.name().contains(mm.deserialize("s remaining"))) {
+                            bar.progress(getProgress(sec[0].get(), max));
+                        }
+                    }
+
                     sec[0].getAndIncrement();
-                },
-                0, 20);
+                }
+        * */
     }
 
-    public static BossBar updateBossBar(int sec, int max) {
-        return BossBar.bossBar(
+    public static BossBar updateBossBar(int sec, int max, @Nullable BossBar current) {
+        if (current == null)
+            return BossBar.bossBar(
                 mm.deserialize(max - sec + "s remaining"),
                 getProgress(sec, max),
                 BossBar.Color.YELLOW,
-                BossBar.Overlay.NOTCHED_10
-        );
+                BossBar.Overlay.PROGRESS
+            );
+        else {
+            current.name(mm.deserialize(max - sec + "s remaíning"));
+            current.progress(getProgress(sec, max));
+            return current;
+        }
     }
 
     public static float getProgress(int sec, int max) {
